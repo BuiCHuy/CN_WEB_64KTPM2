f36c4084537908715e165984b0a64794f6e54df9
BuiCHuy <167603962+BuiCHuy@users.noreply.github.com>
2024-11-25 14:23:59 +0700

    Delete BTTH/TH1_b1 directory

--- BTTH/TH1_b1/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $index=$_POST["index"];
-    unset($_SESSION["flowers"][$index]);
-    header("Location: index.php");
-    exit;
-}
